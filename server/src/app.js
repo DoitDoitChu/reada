8c68efae0f2a18ef7b731bf53df09a738fc57e22
Jane Lee <Yesdoing13@gmail.com>
2020-02-13 00:22:06 +0900

    feat: add login controller

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 app.use(morgan('combined'));
--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,6 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 export const join = (req, res) => res.json({ message: 'Join' });
-export const login = (req, res) => res.json({ message: 'login' });
+export const login = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    return res.status(400).json({ success: false, error: 'login failure' });
+  }
+  return res.status(200).json({ success: true, message: 'login success' });
+};
 export const logout = (req, res) => res.json({ message: 'logout' });
 export const users = (req, res) => res.json({ message: 'users' });
 export const userDetail = (req, res) => res.json({ message: 'userDetail' });
