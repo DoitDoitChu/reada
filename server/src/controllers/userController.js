1abdc00359eacae68f16687c935ef695bb4da9e2
Jane Lee <Yesdoing13@gmail.com>
2020-02-22 15:19:44 +0900

    feat: add user join logic

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,12 +1,29 @@
+import passport from 'passport';
+import User from '../models/User';
+
 /* eslint-disable import/prefer-default-export */
-export const join = (req, res) => res.json({ message: 'Join' });
-export const login = (req, res) => {
+export const join = async (req, res) => {
   const {
     body: { name, email, password, password2 }
   } = req;
+
   if (password !== password2) {
-    return res.status(400).json({ success: false, error: 'login failure' });
+    return res.status(400).json({ success: false, error: 'two password different' });
   }
+
+  try {
+    const user = await User({
+      name,
+      email
+    });
+    await User.register(user, password);
+  } catch (error) {
+    return res.status(500).json({ success: false, error: 'Sorry, we have some errors' });
+  }
+
+  return res.status(200).json({ success: true, message: 'Join success' });
+};
+export const login = (req, res) => {
   return res.status(200).json({ success: true, message: 'login success' });
 };
 export const logout = (req, res) => res.json({ message: 'logout' });
