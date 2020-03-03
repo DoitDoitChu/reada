a758e98eb355962d13ae4bcf600bf947df5884d0
서한샘 <saem@seohansaem-ui-MacBookPro.local>
2020-03-03 14:48:19 +0900

    fix:webpack version fix

--- client/src/pages/Home/HomePresenter.js
+++ client/src/pages/Home/HomePresenter.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const HomePresenter = () => <div>Home Page;</div>;
+const HomePresenter = () => (
+  <div>
+    <div id="header">
+      <div id="title"></div>
+      <div id="login"></div>
+      <div id="join"></div>
+      <div id="searchBar"></div>
+      <h1>test</h1>
+    </div>
+    <div id="body"></div>
+    <div id="footer"></div>
+  </div>
+);
 
 export default HomePresenter;
