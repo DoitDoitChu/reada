75db9934f684bd04ef24ba8d1c95571dd3ffa9cc
서한샘 <saem@seohansaem-ui-MacBookPro.local>
2020-03-08 19:47:34 +0900

    feat: background revise

--- client/src/assets/theme.js
+++ client/src/assets/theme.js
@@ -4,8 +4,11 @@ const theme = {
     black: '#000',
     lightGray: '#F8F8F8',
     darkGray: '#707070',
+    veryDarkGray: '#333333',
+    lightGrayishBlue: '#f0f1f4',
     darkGrayishBlue: '#303538',
     vividBlue: '#1F8CE6',
+    softOrange: '#E7B366',
   },
   size: {
     small: '76px',
