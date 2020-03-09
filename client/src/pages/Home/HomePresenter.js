0063fd656f4eda6245eaa9700d557e1b2f9c6fd7
dosaem <dosaem90@gmail.com>
2020-03-09 19:30:00 +0900

    feat: popularsubject add

--- client/src/pages/Home/HomePresenter.js
+++ client/src/pages/Home/HomePresenter.js
@@ -8,27 +8,38 @@ import {
   Login,
   SearchBar,
   HeaderContent,
-  HeaderTopWrapper,
-  HeaderBottomWrapper,
+  BodyWrapper,
+  PopularSubjectsWrapper,
+  PopularSubjectsTitle,
+  PopularSubjectContentWrapper,
+  PopularSubjectContent,
 } from '../components/Style';
 
 const HomePresenter = () => (
   <Wrapper>
     <HeaderWrapper>
       <HeaderContent>
-        <HeaderTopWrapper>
-          <Title></Title>
-          <ButtonWrapper>
-            <Join>회원가입</Join>
-            <Login>로그인</Login>
-          </ButtonWrapper>
-        </HeaderTopWrapper>
-        <HeaderBottomWrapper>
-          <SearchBar />
-        </HeaderBottomWrapper>
+        <Title></Title>
+        <SearchBar />
+        <ButtonWrapper>
+          <Join>회원가입</Join>
+          <Login>로그인</Login>
+        </ButtonWrapper>
       </HeaderContent>
     </HeaderWrapper>
-    <div id="body"></div>
+    <BodyWrapper>
+      <PopularSubjectsWrapper>
+        <PopularSubjectsTitle>인기 카테고리</PopularSubjectsTitle>
+        <PopularSubjectContentWrapper>
+          <PopularSubjectContent>소설</PopularSubjectContent>
+          <PopularSubjectContent>경영/경제</PopularSubjectContent>
+          <PopularSubjectContent>인문/사회/역사</PopularSubjectContent>
+          <PopularSubjectContent>자기계발</PopularSubjectContent>
+          <PopularSubjectContent>에세이/시</PopularSubjectContent>
+          <PopularSubjectContent>과학</PopularSubjectContent>
+        </PopularSubjectContentWrapper>
+      </PopularSubjectsWrapper>
+    </BodyWrapper>
     <div id="footer"></div>
   </Wrapper>
 );
--- client/src/pages/components/Style.js
+++ client/src/pages/components/Style.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import img from '../../image/background.png';
 import logo from '../../image/logo.png';
 
@@ -13,6 +13,7 @@ export const Wrapper = styled.div`
 
   display: flex;
   flex-direction: column;
+  align-items: center;
   /* flex-wrap: wrap; */
   /* justify-content: flex-start; */
 
@@ -43,7 +44,7 @@ export const HeaderWrapper = styled.div`
   /* align-items: center; */
   /* overflow-y: visible; */
   width: 100%;
-  height: 450px;
+  height: 300px;
 
   background-color: #000000;
   background-image: url(${img});
@@ -61,18 +62,9 @@ export const HeaderContent = styled.div`
   margin: 0 auto;
   width: 1200px;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   margin-top: 30px;
-`;
-export const HeaderTopWrapper = styled.div`
-  display: flex;
-  margin: 0px 20px;
   justify-content: space-between;
-`;
-export const HeaderBottomWrapper = styled.div`
-  display: flex;
-  /* align-items: center; */
-  margin: 50px auto;
 `;
 
 export const ButtonWrapper = styled.div`
@@ -91,12 +83,14 @@ export const Title = styled.div`
   background-repeat: no-repeat;
   background-position: center center;
   background-size: contain;
+  margin-left: 10px;
 `;
 
 export const SearchBar = styled.input`
   display: flex;
   width: 640px;
   height: 40px;
+  /* margin-left: 70px; */
   border: ${props => `2px solid ${props.theme.color.darkgray}`};
   border-radius: 5px;
 `;
@@ -120,4 +114,58 @@ export const Join = styled(smallButton)`
   /* background-color: ${props => props.theme.color.white}; */
   /* border: ${props => `1px solid ${props.theme.color.vividBlue}`}; */
   color: ${props => props.theme.color.white};
+`;
+
+export const BodyWrapper = styled.div`
+  width: 1200px;
+  height: 1000px;
+  /* background-color: ${props => props.theme.color.softOrange}; */
+`;
+export const PopularSubjectsWrapper = styled.div`
+  background-color: ${props => props.theme.color.white};
+  width: 1200px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* margin: 45px auto; */
+  margin-top: 45px;
+`;
+
+export const PopularSubjectsTitle = styled.div`
+  display: flex;
+  /* width: 200px;
+  height: 30px; */
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 29px;
+  margin-top: 0;
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+export const PopularSubjectContentWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PopularSubjectContent = styled.div`
+  color: #4c4c4c;
+  background-color: #fff;
+  border: 1px solid #e8e7e7;
+  width: calc(20% - 100px);
+  height: 40px;
+  margin: 10px;
+  padding: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: 500;
+  text-decoration: none;
+  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.1);
 `;
