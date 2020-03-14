0dc18d6281b6816f7d8fcbf50fdb012cd37603e2
dosaem <dosaem90@gmail.com>
2020-03-14 16:50:08 +0900

    feat: register page add

--- client/src/pages/Register/RegisterPresenter.js
+++ client/src/pages/Register/RegisterPresenter.js
@@ -1,5 +1,43 @@
 import React from 'react';
-
-const RegisterPresenter = () => <div>Register Page;</div>;
+import {
+  Wrapper,
+  HeaderWrapperBasic,
+  HeaderContent,
+  Title,
+  ButtonWrapper,
+  Join,
+  Login,
+  SearchBarBasic,
+  BodyWrapper,
+  JoinWrapper,
+  JoinTitle,
+  InputTitle,
+  Input,
+  JoinButton,
+} from '../components/Style';
+const RegisterPresenter = () => (
+  <Wrapper>
+    <HeaderWrapperBasic>
+      <HeaderContent>
+        <Title></Title>
+        <ButtonWrapper>
+          <Join>회원가입</Join>
+          <Login>로그인</Login>
+        </ButtonWrapper>
+      </HeaderContent>
+      <SearchBarBasic />
+    </HeaderWrapperBasic>
+    <BodyWrapper>
+      <JoinWrapper>
+        <JoinTitle>회원가입</JoinTitle>
+        <Input placeholder="이름"></Input>
+        <Input placeholder="이메일"></Input>
+        <Input placeholder="비밀번호"></Input>
+        <Input placeholder="비밀번호확인"></Input>
+        <JoinButton>가입하기</JoinButton>
+      </JoinWrapper>
+    </BodyWrapper>
+  </Wrapper>
+);
 
 export default RegisterPresenter;
