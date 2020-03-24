e226c86d2f044c00136890f2af956de30bfc1443
yesdoing <fkrmtleo12@gmail.com>
2020-03-25 00:55:08 +0900

    feat: add Review Controllers and Router

--- server/src/controllers/reviewController.js
+++ server/src/controllers/reviewController.js
@@ -1,3 +1,80 @@
-export const add = (req, res) => res.json({ message: 'add Review' });
-export const edit = (req, res) => res.json({ message: 'edit Review' });
-export const remove = (req, res) => res.json({ message: 'delete Review' });
+import { Types } from 'mongoose';
+import Review from '../models/Review';
+import Book from '../models/Book';
+import User from '../models/User';
+import logger from '../util/logger';
+
+export const add = async (req, res) => {
+  const { comment, grade, bookId } = req.body;
+
+  if (!req.user) {
+    res.status(400).json({ success: false, message: 'Not Login', result: null });
+    return;
+  }
+
+  try {
+    const book = await Book.findById(bookId);
+    const user = await User.findById(req.user.id);
+    const review = new Review({
+      comment,
+      grade,
+      bookId,
+      user: req.user._id
+    });
+
+    book.reviews.push(review);
+    user.reviews.push(review);
+    await book.save();
+    await user.save();
+    res.status(200).json({ success: true, message: 'add review success', result: review });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      message: error,
+      result: null
+    });
+  }
+};
+
+export const edit = async (req, res) => {
+  const { ObjectId } = Types;
+  const { commentId } = req.params;
+  const { comment, grade, book, userId } = req.body;
+
+  if (!ObjectId.isValid(commentId)) {
+    res.status(400);
+    return;
+  }
+
+  if (!ObjectId.isValid(userId)) {
+    res.status(400);
+    return;
+  }
+
+  let reveiw = null;
+
+  try {
+    reveiw = await Review.findByIdAndUpdate(
+      commentId,
+      { comment, grade, book, user: userId },
+      {
+        new: true
+      }
+    );
+    res.status(200).json({ success: true, message: 'Comment updated', result: review });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Comment didn't updated", result: null });
+  }
+};
+export const remove = async (req, res) => {
+  const { commentId } = req.params;
+
+  try {
+    await Review.findByIdAndDelete(commentId).exec();
+    res.status(204).json({ success: true, message: 'Deleted Review', result: null });
+  } catch (error) {
+    logger(error);
+    res.status(404).json({ success: false, message: error, result: null });
+  }
+};
--- server/src/routes/reviewRouter.js
+++ server/src/routes/reviewRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import passport from 'passport';
 import { add, edit, remove } from '../controllers/reviewController';
 
 const reviewRouter = express.Router();
 
-reviewRouter.post('', add);
-reviewRouter.put('', edit);
-reviewRouter.delete('', remove);
+reviewRouter.post('', passport.authenticate('jwt', { session: false }), add);
+reviewRouter.put('/:commentId', passport.authenticate('jwt', { session: false }), edit);
+reviewRouter.delete('/:commentId', passport.authenticate('jwt', { session: false }), remove);
 
 export default reviewRouter;
